d13617e9168fbe0c8d34f5147abacc3f2512cca3
KyleAsaff <kyleasaff@gmail.com>
2015-03-07 15:22:45 -0400

    Added basic HTTP authentication
    Basic HTTP authentication for editing and deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+	http_basic_authenticate_with name: "kyle", password: "112233", only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
